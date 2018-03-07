da4b5231f89f1eb5dd1b5c6d7c719214b041710c
Sam Goldstein <git@samuelgoldstein.com>
2018-03-07 12:46:04 -0800

    Initial commit

---
+++ Lab5_Client/Lab5_Client/Lab5_Client.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lab5_Client
+//
+//  Created by Sam Goldstein on 3/7/18.
+//  Copyright © 2018 Sam Goldstein. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
